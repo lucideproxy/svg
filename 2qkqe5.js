2ef9d802f7cb8d8231af45567b7d8ab85d091fb3
coinbaselarper <275395376+coinbaselarper@users.noreply.github.com>
2026-07-07 21:53:41 -0400

    publish svg v4 (index.svg + logo.svg)

--- 2qkqe5.js
+++ 2qkqe5.js
@@ -1,10 +1,10 @@
-importScripts("smqbj/fofore.js");
+importScripts("ze1cr/wd2w8g.js");
 
 self.addEventListener("install", () => self.skipWaiting());
 self.addEventListener("activate", (event) => event.waitUntil(self.clients.claim()));
 
 self.addEventListener("fetch", (event) => {
-  if (_l8dbpfs.shouldRoute(event)) {
-    event.respondWith(_l8dbpfs.route(event));
+  if (_0iqr11j.shouldRoute(event)) {
+    event.respondWith(_0iqr11j.route(event));
   }
 });

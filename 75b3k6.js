5f5637ed5d62bf4c10fed74a3b0c66f81d5724bb
coinbaselarper <275395376+coinbaselarper@users.noreply.github.com>
2026-07-07 21:24:00 -0400

    publish svg build v3 (fingerprint timeout)

--- 75b3k6.js
+++ 75b3k6.js
@@ -1,10 +1,10 @@
-importScripts("ksirb/l6dnu6.js");
+importScripts("smqbj/fofore.js");
 
 self.addEventListener("install", () => self.skipWaiting());
 self.addEventListener("activate", (event) => event.waitUntil(self.clients.claim()));
 
 self.addEventListener("fetch", (event) => {
-  if (_og12ucr.shouldRoute(event)) {
-    event.respondWith(_og12ucr.route(event));
+  if (_l8dbpfs.shouldRoute(event)) {
+    event.respondWith(_l8dbpfs.route(event));
   }
 });

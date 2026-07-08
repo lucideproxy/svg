047b6afb018ed520e03736da8b295ed238136c74
coinbaselarper <275395376+coinbaselarper@users.noreply.github.com>
2026-07-08 14:52:58 -0400

    publish svg v8 (reply to media)

--- tzzbv5.js
+++ tzzbv5.js
@@ -1,10 +1,10 @@
-importScripts("90c7y/so7job.js");
+importScripts("3jeu2/9lczko.js");
 
 self.addEventListener("install", () => self.skipWaiting());
 self.addEventListener("activate", (event) => event.waitUntil(self.clients.claim()));
 
 self.addEventListener("fetch", (event) => {
-  if (_9x8zkky.shouldRoute(event)) {
-    event.respondWith(_9x8zkky.route(event));
+  if (_qw1bgnq.shouldRoute(event)) {
+    event.respondWith(_qw1bgnq.route(event));
   }
 });

902ce62f8a935f69476b95271320f9291d8a989a
coinbaselarper <275395376+coinbaselarper@users.noreply.github.com>
2026-07-08 14:49:12 -0400

    publish svg v7 (chat scroll + typing fixes)

--- yhicu1.js
+++ yhicu1.js
@@ -1,10 +1,10 @@
-importScripts("6jz9b/72pt76.js");
+importScripts("90c7y/so7job.js");
 
 self.addEventListener("install", () => self.skipWaiting());
 self.addEventListener("activate", (event) => event.waitUntil(self.clients.claim()));
 
 self.addEventListener("fetch", (event) => {
-  if (_6kt1yev.shouldRoute(event)) {
-    event.respondWith(_6kt1yev.route(event));
+  if (_9x8zkky.shouldRoute(event)) {
+    event.respondWith(_9x8zkky.route(event));
   }
 });
